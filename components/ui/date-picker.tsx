50231c31bb076a5addea6c24a91d8e5361281611
Kareem Abbas <kareemabbas90@gmail.com>
2026-09-04 06:20:32 +0300

    fix: remove initialFocus from react-day-picker in date-picker.tsx

--- components/ui/date-picker.tsx
+++ components/ui/date-picker.tsx
@@ -46,7 +46,6 @@ export function DatePicker({
           mode="single"
           selected={date}
           onSelect={onChange}
-          initialFocus
         />
       </PopoverContent>
     </Popover>
